feb8cbcf99d4c48e31f84f43cf53a61df76738b5
MPaladin16 <mp52512@fer.hr>
2021-12-14 14:50:34 +0100

    1. minigame almost done
    tn nn

--- ORDI - Escape room/Assets/SampleScenes/Scripts/DotGame.cs
+++ ORDI - Escape room/Assets/SampleScenes/Scripts/DotGame.cs
@@ -6,76 +6,71 @@ using UnityEngine.UI;
 public class DotGame : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Material Material1;
-    public Material Material2;
     public Canvas Canvas; 
 
-    public Button[] buttons;
+    public List<Button> buttons;
 
 
 
     void Start()
     {
-        Button button0 = GameObject.Find("button (0)").GetComponent<Button>();
-        Debug.Log(button0.GetComponent<Image>().color);
-        Debug.Log(button0);
+        Button button0 = Canvas.transform.GetChild(1).gameObject.GetComponent<Button>();
+        Button button1 = Canvas.transform.GetChild(2).gameObject.GetComponent<Button>();
+        Button button2 = Canvas.transform.GetChild(3).gameObject.GetComponent<Button>();
+        Button button3 = Canvas.transform.GetChild(4).gameObject.GetComponent<Button>();
+        Button button4 = Canvas.transform.GetChild(5).gameObject.GetComponent<Button>();
+        Button button5 = Canvas.transform.GetChild(8).gameObject.GetComponent<Button>();
+        Button button6 = Canvas.transform.GetChild(6).gameObject.GetComponent<Button>();
+        Button button7 = Canvas.transform.GetChild(9).gameObject.GetComponent<Button>();
+        Button button8 = Canvas.transform.GetChild(7).gameObject.GetComponent<Button>();
+        Button button9 = Canvas.transform.GetChild(10).gameObject.GetComponent<Button>();
+        Button button10 = Canvas.transform.GetChild(13).gameObject.GetComponent<Button>();
+        Button button11 = Canvas.transform.GetChild(11).gameObject.GetComponent<Button>();
+        Button button12 = Canvas.transform.GetChild(14).gameObject.GetComponent<Button>();
+        Button button13 = Canvas.transform.GetChild(12).gameObject.GetComponent<Button>();
+        Button button14 = Canvas.transform.GetChild(15).gameObject.GetComponent<Button>();
+        Button button15 = Canvas.transform.GetChild(18).gameObject.GetComponent<Button>();
+        Button button16 = Canvas.transform.GetChild(16).gameObject.GetComponent<Button>();
+        Button button17 = Canvas.transform.GetChild(19).gameObject.GetComponent<Button>();
+        Button button18 = Canvas.transform.GetChild(17).gameObject.GetComponent<Button>();
+        Button button19 = Canvas.transform.GetChild(20).gameObject.GetComponent<Button>();
+        Button button20 = Canvas.transform.GetChild(23).gameObject.GetComponent<Button>();
+        Button button21 = Canvas.transform.GetChild(21).gameObject.GetComponent<Button>();
+        Button button22 = Canvas.transform.GetChild(24).gameObject.GetComponent<Button>();
+        Button button23 = Canvas.transform.GetChild(22).gameObject.GetComponent<Button>();
+        Button button24 = Canvas.transform.GetChild(25).gameObject.GetComponent<Button>();
 
-        Button button1 = GameObject.Find("button (1)").GetComponent<Button>();
-        Button button2 = GameObject.Find("button (2)").GetComponent<Button>();
-        Button button3 = GameObject.Find("button (3)").GetComponent<Button>();
-        Button button4 = GameObject.Find("button (4)").GetComponent<Button>();
-        Button button5 = GameObject.Find("button (5)").GetComponent<Button>();
-        Button button6 = GameObject.Find("button (6)").GetComponent<Button>();
-        Button button7 = GameObject.Find("button (7)").GetComponent<Button>();
-        Button button8 = GameObject.Find("button (8)").GetComponent<Button>();
-        Button button9 = GameObject.Find("button (9)").GetComponent<Button>();
-        Button button10 = GameObject.Find("button (10)").GetComponent<Button>();
-        Button button11 = GameObject.Find("button (11)").GetComponent<Button>();
-        Button button12 = GameObject.Find("button (12)").GetComponent<Button>();
-        Button button13 = GameObject.Find("button (13)").GetComponent<Button>();
-        Button button14 = GameObject.Find("button (14)").GetComponent<Button>();
-        Button button15 = GameObject.Find("button (15)").GetComponent<Button>();
-        Button button16 = GameObject.Find("button (16)").GetComponent<Button>();
-        Button button17 = GameObject.Find("button (17)").GetComponent<Button>();
-        Button button18 = GameObject.Find("button (18)").GetComponent<Button>();
-        Button button19 = GameObject.Find("button (19)").GetComponent<Button>();
-        Button button20 = GameObject.Find("button (20)").GetComponent<Button>();
-        Button button21 = GameObject.Find("button (21)").GetComponent<Button>();
-        Button button22 = GameObject.Find("button (22)").GetComponent<Button>();
-        Button button23 = GameObject.Find("button (23)").GetComponent<Button>();
-        Button button24 = GameObject.Find("button (24)").GetComponent<Button>();
+        buttons.Add(button0);
+        buttons.Add(button1);
+        buttons.Add(button2);
+        buttons.Add(button3);
+        buttons.Add(button4);
+        buttons.Add(button5);
+        buttons.Add(button6);
+        buttons.Add(button7);
+        buttons.Add(button8);
+        buttons.Add(button9);
+        buttons.Add(button10);
+        buttons.Add(button11);
+        buttons.Add(button12);
+        buttons.Add(button13);
+        buttons.Add(button14);
+        buttons.Add(button15);
+        buttons.Add(button16);
+        buttons.Add(button17);
+        buttons.Add(button18);
+        buttons.Add(button19);
+        buttons.Add(button20);
+        buttons.Add(button21);
+        buttons.Add(button22);
+        buttons.Add(button23);
+        buttons.Add(button24);
 
-        buttons[0] = button0;
-        buttons[1] = button1;
-        buttons[2] = button2;
-        buttons[3] = button3;
-        buttons[4] = button4;
-        buttons[5] = button5;
-        buttons[6] = button6;
-        buttons[7] = button7;
-        buttons[8] = button8;
-        buttons[9] = button9;
-        buttons[10] = button10;
-        buttons[11] = button11;
-        buttons[12] = button12;
-        buttons[13] = button13;
-        buttons[14] = button14;
-        buttons[15] = button15;
-        buttons[16] = button16;
-        buttons[17] = button17;
-        buttons[18] = button18;
-        buttons[19] = button19;
-        buttons[20] = button20;
-        buttons[21] = button21;
-        buttons[22] = button22;
-        buttons[23] = button23;
-        buttons[24] = button24;
-
-        foreach (Button btn in buttons) {
+        foreach (Button btn in buttons)
+        {
             btn.GetComponent<Image>().color = Color.red;
             btn.onClick.AddListener(() => ChangeColor(btn));
         }
-
 
 
         int xcount = Random.Range(0, 25);
@@ -102,10 +97,29 @@ public class DotGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int done = 0;
+        foreach (Button btn in buttons) {
+            if (btn.GetComponent<Image>().color == Color.green) {
+                done++;
+            }
+        }
+        if (done == 25)
+        {
+
+        }
+
     }
 
     void ChangeColor(Button btn)
     {
+        int y;
+        if (btn.name.Length == 10)
+        {
+            y = int.Parse(btn.name.Substring(8, 1));
+        }
+        else {
+            y = int.Parse(btn.name.Substring(8, 2));
+        }
         if (btn.GetComponent<Image>().color == Color.red)
         {
             btn.GetComponent<Image>().color = Color.green;
@@ -113,6 +127,62 @@ public class DotGame : MonoBehaviour
         else {
             btn.GetComponent<Image>().color = Color.red;
         }
+        if (y > 0)
+        {
+            if (buttons[y - 1] != null)
+            {
+                if (buttons[y - 1].GetComponent<Image>().color == Color.red)
+                {
+                    buttons[y - 1].GetComponent<Image>().color = Color.green;
+                }
+                else
+                {
+                    buttons[y - 1].GetComponent<Image>().color = Color.red;
+                }
+            }
+        }
+        if (y < 24)
+        {
+            if (buttons[y + 1] != null)
+            {
+                if (buttons[y + 1].GetComponent<Image>().color == Color.red)
+                {
+                    buttons[y + 1].GetComponent<Image>().color = Color.green;
+                }
+                else
+                {
+                    buttons[y + 1].GetComponent<Image>().color = Color.red;
+                }
+            }
+        }
+        if (y > 4)
+        {
+            if (buttons[y- 5] != null)
+            {
+                if (buttons[y - 5].GetComponent<Image>().color == Color.red)
+                {
+                    buttons[y - 5].GetComponent<Image>().color = Color.green;
+                }
+                else
+                {
+                    buttons[y - 5].GetComponent<Image>().color = Color.red;
+                }
+            }
+        }
+        if (y < 20) { 
+            if (buttons[y + 5] != null)
+            {
+                if (buttons[y + 5].GetComponent<Image>().color == Color.red)
+                {
+                    buttons[y + 5].GetComponent<Image>().color = Color.green;
+                }
+                else
+                {
+                    buttons[y + 5].GetComponent<Image>().color = Color.red;
+                }
+            } }
+
+
 
     }
 
