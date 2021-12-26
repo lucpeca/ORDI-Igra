fb98cd5f47b1a2fd812722bc8de2ce2ee40b4867
MPaladin16 <mp52512@fer.hr>
2021-12-26 17:37:44 +0100

    jos malo skripte za info o stvarima

--- ORDI - Escape room/Assets/Scripts/MinigameStarter.cs
+++ ORDI - Escape room/Assets/Scripts/MinigameStarter.cs
@@ -8,12 +8,33 @@ public class MinigameStarter : MonoBehaviour
     public Canvas Canvas1;
     public Canvas Canvas2;
     public Canvas Canvas3;
+    public Canvas InfoCanvas;
     public GameObject Minigame1;
     public GameObject Minigame2;
     public GameObject Minigame3;
+
+
+
     private bool entered1 = false;
     private bool entered2 = false;
     private bool entered3 = false;
+
+    private bool info1 = false;
+    private bool info2 = false;
+    private bool info3 = false;
+    private bool info4 = false;
+    private bool info5 = false;
+    private bool info6 = false;
+    private bool info7 = false;
+    private bool info8 = false;
+    private bool info9 = false;
+    private bool info10 = false;
+    private bool info11 = false;
+    private bool info12 = false;
+    private bool info13 = false;
+    private bool info14 = false;
+    private bool info15 = false;
+
 
 
     // Start is called before the first frame update
@@ -60,6 +81,13 @@ public class MinigameStarter : MonoBehaviour
             Canvas3.gameObject.transform.GetChild(3).GetComponent<MovePlayer>().enabled = true;
             Canvas3.gameObject.SetActive(true);
         }
+        if (info1 == true && Input.GetKeyDown("e"))
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+            this.transform.GetChild(1).GetComponent<MouseLook>().enabled = false;
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            InfoCanvas.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
 
 
 
@@ -82,6 +110,31 @@ public class MinigameStarter : MonoBehaviour
             GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
             entered3 = true;
         }
+        if (other.gameObject.name.Contains("axe")) {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info1 = true;
+        }
+        if (other.gameObject.name.Contains("shield"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info2 = true;
+        }
+        if (other.gameObject.name.Contains("glass"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info3 = true;
+        }
+        if (other.gameObject.name.Contains("jug"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info4 = true;
+        }
+        if (other.gameObject.name.Contains("Plate"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info5 = true;
+        }
+        // -> TO DO ostalih 10 predmeta 
 
     }
 
@@ -104,6 +157,32 @@ public class MinigameStarter : MonoBehaviour
             Canvas3.gameObject.SetActive(false);
             entered3 = false;
         }
+        if (other.gameObject.name.Contains("axe"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info1 = false;
+        }
+        if (other.gameObject.name.Contains("shield"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info2 = false;
+        }
+        if (other.gameObject.name.Contains("glass"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info3 = false;
+        }
+        if (other.gameObject.name.Contains("jug"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info4 = false;
+        }
+        if (other.gameObject.name.Contains("Plate"))
+        {
+            GameObject.Find("Canvas").gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            info5 = false;
+        }
+        // To Do -> ostalih 10 predmeta
     }
 
 
